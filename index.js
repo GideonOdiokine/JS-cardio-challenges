844700e89737d00d93ae03521a30a07f448d078e
GideonOdiokine <gidysmartodiokineh@gmail.com>
2021-11-22 00:23:20 +0100

    added isPalindrome function

--- index.js
+++ index.js
@@ -47,7 +47,13 @@ function reverseString(str) {
 // Return true if palindrome and false if not
 // ex. isPalindrome('racecar') === 'true', isPalindrome('hello') == false
 
-function isPalindrome(str) { }
+function isPalindrome(str) {
+    if (str === str.split('').reverse().join('')) {
+        return true
+    } else {
+        return false
+    }
+}
 
 
 
@@ -80,6 +86,6 @@ function fizzBuzz() { }
 
 
 // Call Function
-const output = reverseString('hello');
+const output = isPalindrome('noon');
 
 console.log(output);
